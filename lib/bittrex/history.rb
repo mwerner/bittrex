c57a84cb1ca69c69fc5a5ee6f7f4fd2258f93753
Roshan <icemission@gmail.com>
2017-07-25 11:23:38 +0200

    Added get specific market history

---
+++ lib/bittrex/history.rb
@@ -0,0 +1,22 @@
+module Bittrex
+  class History
+    attr_reader :created_at, :price, :total, :raw
+
+    def initialize(attrs = {})
+      @created_at = Time.parse(attrs['TimeStamp'])
+      @price = attrs['Price']
+      @total = attrs['Total']
+      @raw = attrs
+    end
+
+    def self.get(market)
+      client.get('public/getmarkethistory',{market: market})).map{|data| new(data) }
+    end
+
+    private
+
+    def self.client
+      @client ||= Bittrex.client
+    end
+  end
+end
