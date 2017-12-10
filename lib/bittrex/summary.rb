a3425346d0c948b21fdabcbd4f7e55490625bc0a
Michael Lang <mwlang@cybrains.net>
2017-12-09 22:16:20 -0600

    Bittrex::Summary new attributes: :bid, :ask, :open_buy_orders, :open_sell_orders, :previous_day, :updated_at (#18)

--- lib/bittrex/summary.rb
+++ lib/bittrex/summary.rb
@@ -3,19 +3,26 @@ module Bittrex
     include Helpers
 
     attr_reader :name, :high, :low, :volume, :last, :base_volume, :raw, :created_at
+    attr_reader :bid, :ask, :open_buy_orders, :open_sell_orders, :previous_day, :updated_at
 
     alias_method :vol, :volume
     alias_method :base_vol, :base_volume
 
     def initialize(attrs = {})
-      @name        = attrs['MarketName']
-      @high        = attrs['High']
-      @low         = attrs['Low']
-      @volume      = attrs['Volume']
-      @last        = attrs['Last']
-      @base_volume = attrs['BaseVolume']
-      @raw         = attrs
-      @created_at  = extract_timestamp(attrs['TimeStamp'])
+      @name             = attrs['MarketName']
+      @high             = attrs['High']
+      @low              = attrs['Low']
+      @volume           = attrs['Volume']
+      @last             = attrs['Last']
+      @base_volume      = attrs['BaseVolume']
+      @bid              = attrs['Bid']
+      @ask              = attrs['Ask']
+      @open_buy_orders  = attrs['OpenBuyOrders']
+      @open_sell_orders = attrs['OpenSellOrders']
+      @previous_day     = attrs['PrevDay']
+      @updated_at       = extract_timestamp(attrs['TimeStamp'])
+      @created_at       = extract_timestamp(attrs['Created'])
+      @raw              = attrs
     end
 
     def self.all
--- spec/models/summary_spec.rb
+++ spec/models/summary_spec.rb
@@ -11,6 +11,12 @@ describe Bittrex::Summary do
     it { should_assign_attribute(subject, :volume, 1406611.43827056) }
     it { should_assign_attribute(subject, :last, 0.00000020) }
     it { should_assign_attribute(subject, :base_volume, 0.30179011) }
-    it { should_assign_attribute(subject, :created_at, Time.parse('2014-06-26T05:22:57.673')) }
+    it { should_assign_attribute(subject, :ask, 2.3e-07) }
+    it { should_assign_attribute(subject, :bid, 2.0e-07) }
+    it { should_assign_attribute(subject, :open_buy_orders, 7) }
+    it { should_assign_attribute(subject, :open_sell_orders, 8) }
+    it { should_assign_attribute(subject, :previous_day, 2.2e-07) }
+    it { should_assign_attribute(subject, :updated_at, Time.parse('2014-06-26T05:22:57.673')) }
+    it { should_assign_attribute(subject, :created_at, Time.parse('2014-03-01T21:00:00.000')) }
   end
 end
